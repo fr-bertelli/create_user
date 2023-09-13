12dff55cf5c777c0b5a485cf03fed5c57dda0be6
Bertelli <frbertelli@rd.com.br>
2023-09-13 19:01:34 -0300

    Fix Host

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def delete_pessoas(id):
     return jsonify({'message': f'Erro na exclusão: {retorno}'})
 
 
-app.run(port=5000,host='127.0.0.1',debug=False)
+app.run(port=5000,host='0.0.0.0',debug=False)
